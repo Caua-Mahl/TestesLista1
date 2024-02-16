eb9079078440d03ad04a82083b7be634d090f04c
Cauã Mahl <cauamahl@yahoo.com.br>
2024-02-16 11:47:07 -0300

    5 pronta

--- src/ListaNumerica.php
+++ src/ListaNumerica.php
@@ -1,12 +1,61 @@
-<?php 
+<?php
 
 namespace src;
 
+use function PHPUnit\Framework\isEmpty;
+
 class ListaNumerica
 {
-    public static function SomarElementos(array $array)
+    public static function somarElementos(array $array): int
     {
         return array_sum($array);
     }
 
+    public static function encontrarMaiorElemento(array $array): int|bool
+    {
+        if (empty($array)) {
+            return False;
+        }
+
+        return max($array);
+    }
+
+    public static function encontrarMenorElemento(array $array): int|bool
+    {
+        if (empty($array)) {
+            return False;
+        }
+
+        return min($array);
+    }
+
+    public static function ordenarLista(array $array): array
+    {
+        if (empty($array)) {
+            return [];
+        }
+
+        sort($array);
+        return $array;
+    }
+
+    public static function filtrarNumerosPares(array $array): array
+    {
+        if (empty($array)) 
+        {
+            return [];
+        }
+
+        $pares= [];
+
+        for($i=0; $i<count($array); $i++)
+        {
+            if($array[$i] % 2 == 0)
+            {
+                $pares[] = $array[$i];
+            }
+        }
+
+        return $pares;
+    }
 }
--- tests/ListaNumericaTest.php
+++ tests/ListaNumericaTest.php
@@ -7,11 +7,47 @@ require_once "src/ListaNumerica.php";
 
 class ListaNumericaTest extends TestCase
 {
-    public function testListaNumerica()
+    public function testSomarElementos()
     {
-        $this->assertEquals(10, ListaNumerica::SomarElementos([1, 4, 5]),    'Somando errado');
-        $this->assertEquals(10, ListaNumerica::SomarElementos([1, 4, 0, 5]), 'Somando errado com 0 dentro da array');
-        $this->assertEquals(0, ListaNumerica::SomarElementos([0, 0, 0]),    'Somando errado array de zeros');
-
+        $this->assertSame(10,         ListaNumerica::somarElementos([1, 4, 5]),          'Somando errado positivos');
+        $this->assertSame(10,         ListaNumerica::somarElementos([1, 4, 0, 5]),       'Somando errado com 0 dentro da array');
+        $this->assertSame(-10,        ListaNumerica::somarElementos([-1, -4, -5]),       'Somando errado negativos');
+        $this->assertSame(0,          ListaNumerica::somarElementos([5, 0, -5]),         'Somando errado negativos com positivos');
+        $this->assertSame(0,          ListaNumerica::somarElementos([0, 0, 0]),          'Somando errado array de zeros');
+        $this->assertSame(0,          ListaNumerica::somarElementos([]),                 'Somando errado com lista vazia');
+        $this->assertSame(0,          ListaNumerica::somarElementos([1.1, -1.1]),        'Somando errado para números float'); 
     }
+
+    public function testEncontrarMaiorElemento()
+    {
+        $this->assertGreaterThan(1,   ListaNumerica::encontrarMaiorElemento([-1,0,2]),   'Não retorna maior valor');
+        $this->assertFalse(           ListaNumerica::encontrarMaiorElemento([]),         'Não retorna falso ao receber array vazia');
+        $this->assertIsInt(           ListaNumerica::encontrarMaiorElemento([-1,0,2]),   'Não devolve inteiro em array com números');
+    }
+
+    public function testEncontrarMenorElemento()
+    {
+        $this->assertLessThan(0,      ListaNumerica::encontrarMenorElemento([-1,0,2]),   'Não retorna menor valor');
+        $this->assertFalse(           ListaNumerica::encontrarMenorElemento([]),         'Não retorna falso ao receber array vazia');
+        $this->assertIsInt(           ListaNumerica::encontrarMenorElemento([-1,0,2]),   'Não devolve inteiro em array com números');
+    }
+
+    public function testOrdenarLista()
+    {
+        $this->assertIsArray(         ListaNumerica::ordenarLista([4, -1, 0]),           'Não retorna array');
+        $this->assertEmpty(           ListaNumerica::ordenarLista([]),                   'Não retorna array vazia ao receber array vazia');
+        $this->assertSame([-1, 0, 4], ListaNumerica::ordenarLista([4, -1, 0]),           'Não ordena em ordem crescente');
+        $this->assertCount(3,         ListaNumerica::ordenarLista([4, -1, 0]),           'Não retorna array com mesmo tamanho');
+    }
+
+    public function testFiltrarNumerosPares()
+    {
+        $this->assertIsArray(         ListaNumerica::filtrarNumerosPares([1, -2, 0]),    'Não retorna array');
+        $this->assertEmpty(           ListaNumerica::filtrarNumerosPares([]),            'Não retorna array vazia ao receber array vazia');
+        $this->assertEmpty(           ListaNumerica::filtrarNumerosPares([-1,1]),        'Não retorna array vazia ao receber apenas impares');
+        $this->assertSame([-2, 0],    ListaNumerica::filtrarNumerosPares([1, -2, 0]),    'Não retorna apenas pares ou altera a ordem');
+        $this->assertCount(2,         ListaNumerica::filtrarNumerosPares([1, -2, 0]),    'Não retorna array com tamanho correto');
+    }
+
+
 }
